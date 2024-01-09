832695bde0e2bf34daa9762226d081b4e375e58a
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-09 17:01:42 +0100

    ADD > Type_id seeding with Types ids

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -14,11 +15,16 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+
+        $types = Type::all();
+        $ids = $types->pluck('id');
+
         for ($i = 0; $i < 8; $i++) {
             $new_project = new Project();
             $new_project->image = 'https://picsum.photos/id/' . $faker->numberBetween(0,  50) . '/300/200';
             $new_project->description = $faker->paragraph(4);
             $new_project->title = $faker->words(4, true);
+            $new_project->type_id = $faker->randomElement($ids);
             $new_project->save();
         }
     }
